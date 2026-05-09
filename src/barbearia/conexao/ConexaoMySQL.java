dd80b5f1276052f305af9de8fe6aba7d5ef647b2
Gustavo Novellini <gustavonovellini2016@gmail.com>
2026-05-09 01:09:28 -0300

    Atualização pequena

---
+++ src/barbearia/conexao/ConexaoMySQL.java
@@ -0,0 +1,16 @@
+package barbearia.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMySQL {
+
+    private static final String URL     = "jdbc:mysql://localhost:3306/barbearia";
+    private static final String USUARIO = "root";
+    private static final String SENHA   = "sua_senha";
+
+    public static Connection obterConexao() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+}
