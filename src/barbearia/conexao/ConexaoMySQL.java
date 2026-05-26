a0a35679c28d8b03f913cfd391970a905cfd6444
Gustavo Novellini <gustavonovellini2016@gmail.com>
2026-05-26 17:54:02 -0300

    Resolução de bug

--- src/barbearia/conexao/ConexaoMySQL.java
+++ src/barbearia/conexao/ConexaoMySQL.java
@@ -8,7 +8,9 @@ public class ConexaoMySQL {
 
     private static final String URL     = "jdbc:mysql://localhost:3306/barbearia";
     private static final String USUARIO = "root";
+
     private static final String SENHA   = "senha";
+
 
     public static Connection obterConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
